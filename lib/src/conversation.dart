1f013705abd488d33e5f99552d709054adc55a99
ALIAS GEORGE <aliasgeorge9329@gmail.com>
2024-01-07 21:01:35 +0530

    Update conversation.dart

--- lib/src/conversation.dart
+++ lib/src/conversation.dart
@@ -1,17 +1,11 @@
-library flutter_support_chat;
-
-// Flutter imports:
-import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_support_chat/src/text_message.dart';
-import 'chat_header.dart';
 import 'model/chat.dart';
-import 'send_message.dart';
+import 'model/message.dart';
+import 'model/state.dart';
 
-/// `FlutterSupportChatConversation` is should only used in FlutterSupportChat.
-class FlutterSupportChatConversation extends StatefulWidget {
+/// `FlutterSupportChatMessageSend` is should only used in FlutterSupportChat.
+class FlutterSupportChatMessageSend extends StatefulWidget {
   /// `supporterID` is a required list of Ids.
   /// Ids can be Email or FirebaseUsersIds
   /// This Ids are able to view all Cases.
@@ -26,24 +20,12 @@ class FlutterSupportChatConversation extends StatefulWidget {
   /// `firestoreInstance` is required for using firestore
   final FirebaseFirestore firestoreInstance;
 
-  /// `onNewCaseText` is a required String.
-  /// New Cases will have this message by default.
-  /// Message is send by the first supporterID
-  final String onNewCaseText;
-
-  /// `createCaseButtonText` is a optional String.
-  /// This text is shown on the button to create a new Case
-  final String createCaseButtonText;
+  /// `id` is should only used in FlutterSupportChat.
+  final String id;
 
   /// `writeMessageText` is a optional String.
   /// This text is shown on the textfield for new comments
   final String writeMessageText;
-
-  /// `id` is should only used in FlutterSupportChat.
-  final String id;
-
-  /// `back` is should only used in FlutterSupportChat.
-  final Function back;
 
   /// `onNewMessageCreated` is a optional Function.
   /// With this for example you can send a push notification
@@ -51,97 +33,146 @@ class FlutterSupportChatConversation extends StatefulWidget {
 
   final String deviceInfos;
 
-  const FlutterSupportChatConversation({
-    Key? key,
-    required this.id,
-    required this.back,
+  FlutterSupportChatMessageSend({
     required this.supporterID,
     required this.supportSection,
     required this.currentID,
     required this.firestoreInstance,
-    required this.onNewCaseText,
-    required this.createCaseButtonText,
+    required this.id,
     required this.writeMessageText,
     required this.onNewMessageCreated,
     required this.deviceInfos,
-  }) : super(key: key);
+  });
+
   @override
-  _FlutterSupportChatConversationState createState() =>
-      _FlutterSupportChatConversationState();
+  _FlutterSupportChatMessageSendState createState() =>
+      _FlutterSupportChatMessageSendState();
 }
 
-class _FlutterSupportChatConversationState
-    extends State<FlutterSupportChatConversation> {
-  final ScrollController _controller = ScrollController();
+class _FlutterSupportChatMessageSendState
+    extends State<FlutterSupportChatMessageSend> {
+  final TextEditingController _textEditingController = TextEditingController();
+  bool sending = false;
+  bool enabled = false;
+  @override
+  void initState() {
+    checkIfClosed();
+    super.initState();
+  }
+
+  bool get isSupporter => widget.supporterID.contains(
+        widget.currentID,
+      );
+
+  Future<void> checkIfClosed() async {
+    SupportChat c = SupportChat.fromFireStore(
+      await widget.firestoreInstance
+          .collection(
+            widget.supportSection,
+          )
+          .doc(widget.id)
+          .get(),
+    );
+    setState(() {
+      enabled = c.state != SupportCaseState.closed;
+    });
+  }
+
+  send() async {
+    sending = true;
+    setState(() {});
+    final SupportChat c = SupportChat.fromFireStore(
+      await widget.firestoreInstance
+          .collection(
+            widget.supportSection,
+          )
+          .doc(widget.id)
+          .get(),
+    );
+    c.messages.add(
+      SupportChatMessage(
+        content: _textEditingController.text.trim(),
+        sender: widget.currentID,
+        timestamp: Timestamp.now(),
+        deviceInfos: widget.deviceInfos,
+      ),
+    );
+    c.state = isSupporter
+        ? SupportCaseState.waitingForCustomer
+        : SupportCaseState.waitingForSupporter;
+    await c
+        .update(widget.firestoreInstance.collection(
+      widget.supportSection,
+    ))
+        .then((value) {
+      sending = false;
+      _textEditingController.clear();
+      setState(() {});
+      widget.onNewMessageCreated(c);
+    }).onError((error, stackTrace) {
+      print(error.toString());
+      sending = false;
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () => widget.back(),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 5,
-            child: FlutterSupportChatHeaderButton(
-              back: widget.back,
-              currentID: widget.currentID,
-              id: widget.id,
-              supporterID: widget.supporterID,
-            ),
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(
+            left: 10,
+            bottom: 5,
+            top: 5,
+            right: 10,
           ),
-          StreamBuilder<DocumentSnapshot<SupportChat>>(
-            stream: widget.firestoreInstance
-                .collection(
-                  widget.supportSection,
-                )
-                .doc(widget.id)
-                .withConverter<SupportChat>(
-                  fromFirestore: (doc, _) => SupportChat.fromFireStore(doc),
-                  toFirestore: (supportChat, _) => supportChat.toFireStore(),
-                )
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              SupportChat data = snapshot.data!.data()!;
-              Timer(
-                Duration(milliseconds: 100),
-                () => _controller.jumpTo(_controller.position.maxScrollExtent),
-              );
-              return Container(
-                margin: EdgeInsets.fromLTRB(0, 70, 0, 70),
-                child: Scrollbar(
-                  child: ListView.builder(
-                    itemCount: data.messages.length,
-                    shrinkWrap: true,
-                    controller: _controller,
-                    padding: EdgeInsets.only(top: 10, bottom: 10),
-                    itemBuilder: (context, index) {
-                      return TextMessage(
-                        data.messages[index],
-                        currentID: widget.currentID,
-                      );
-                    },
+          width: double.infinity,
+          color: Theme.of(context).bottomAppBarColor,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  controller: _textEditingController,
+                  minLines: 1,
+                  maxLines: 100,
+                  enabled: enabled,
+                  decoration: InputDecoration(
+                    hintText: widget.writeMessageText,
+                    border: InputBorder.none,
                   ),
+                  onChanged: (c) {
+                    setState(() {});
+                  },
                 ),
-              );
-            },
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              if (sending)
+                FloatingActionButton(
+                  onPressed: null,
+                  child: CircularProgressIndicator(),
+                  backgroundColor: Colors.grey,
+                )
+              else
+                FloatingActionButton(
+                  onPressed:
+                      _textEditingController.text.trim().isEmpty ? null : send,
+                  child: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                    size: 18,
+                  ),
+                  backgroundColor: _textEditingController.text.isEmpty
+                      ? Colors.grey
+                      : Colors.red,
+                  elevation: 0,
+                ),
+            ],
           ),
-          FlutterSupportChatMessageSend(
-            currentID: widget.currentID,
-            firestoreInstance: widget.firestoreInstance,
-            id: widget.id,
-            supporterID: widget.supporterID,
-            supportSection: widget.supportSection,
-            writeMessageText: widget.writeMessageText,
-            onNewMessageCreated: widget.onNewMessageCreated,
-            deviceInfos: widget.deviceInfos,
-          ),
-        ],
+        ),
       ),
     );
   }
